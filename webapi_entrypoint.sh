171d7a7539e0d1f7b6d761a4ccc0237ed9acc6af
HouseAlwaysWin <martinwang7963@gmail.com>
2020-12-10 15:23:49 +0800

    Fix entirypoint.sh

--- webapi_entrypoint.sh
+++ webapi_entrypoint.sh
@@ -2,5 +2,5 @@
 
 
 # urls domain must be 0.0.0.0, to expose ports to outside
-run_cmd="dotnet watch -p DapperUnitOfWorkWebApi run"
+run_cmd="dotnet watch -p DapperUnitOfWorkWebApi run --urls=http://0.0.0.0:5001/"
 exec $run_cmd
